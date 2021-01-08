4a99ab26c4a195e6ee2eef3be4ac55625299da48
hannes <hannes.engelhardt@posteo.de>
2021-01-08 19:50:06 +0100

    added scripts for nlp preprocessing

---
+++ run_archives_preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from nlpipe import NlPipe
+import numpy as np
+import os
+import pickle
+from tqdm.auto import tqdm
+
+path = "archive_collection_extracted/"
+stat_df = pd.read_pickle(f"{path}stat_df")
+if os.path.exists(f"{path}text_df"):
+    print("text df found. loading.")
+    text_df = pd.read_pickle(f"{path}text_df")
+    texts = text_df.full_text.to_list()
+    thread_ids = text_df.thread_id.to_list()
+else:
+    thread_ids = stat_df.thread_id.to_list()
+    post_df = pd.read_pickle(f"{path}post_df_extracted")
+    thread_id_of_posts = np.array(post_df.thread_id, dtype=np.uint32)
+    texts = [" ".join(post_df.full_string[thread_id_of_posts == thread_id].tolist()) for thread_id in thread_ids]
+    post_df = None
+    text_df = pd.DataFrame([thread_ids, texts]).transpose()
+    text_df.columns = ['thread_id', 'full_text']
+    text_df.to_pickle(f"{path}text_df")
+
+nlp = NlPipe.NlPipe(texts,path=path, document_ids=thread_ids)
+nlp.preprocess()
