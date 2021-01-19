5d8f2ca8be029d14deb5b4aebf4a63cc97aa9352
hannes <hannes.engelhardt@posteo.de>
2021-01-19 15:52:31 +0100

    small changes

--- run_pol_lda.py
+++ run_pol_lda.py
@@ -17,6 +17,7 @@ if os.path.exists(f"{path}text_df"):
     text_df = pd.read_pickle(f"{path}text_df")
     texts = text_df.full_text.to_list()
     thread_ids = text_df.thread_id.to_list()
+    text_df = None
 else:
     thread_ids = stat_df.thread_id.to_list()
     post_df = pd.read_pickle(f"{path}post_df_extracted")
@@ -31,11 +32,11 @@ nlp = NlPipe.NlPipe(texts, path=path, document_ids=thread_ids, no_processes=36)
 filter_array = np.logical_and(stat_df.thread_id.isin(text_df.sample(frac=0.1, weights=stat_df.replies).thread_id),
                               stat_df.replies > 10)
 filter_array = np.logical_and(filter_array, stat_df.language == 'en')
-nlp.preprocess(load_existing=True)
+nlp.preprocess(load_existing=True, filter_loaded=filter_array)
 nlp.create_bag_of_words(filter_extremes=False, use_phrases='bigram')
 with threadpool_limits(limits=1, user_api='blas'):
     for max_df in tqdm([0.3, 0.2, 0.1], desc="max df"):
-        nlp.filter_extremes(min_df=1, max_df=max_df, keep_n=nlp.keep_n, keep_tokens=nlp.keep_tokens)
+        nlp.filter_extremes(min_df=25, max_df=max_df, keep_n=nlp.keep_n, keep_tokens=nlp.keep_tokens)
         nlp.create_bag_of_words_matrix()
         nlp.search_best_model(topic_list=[50, 100, 150, 200], passes=2,
                               alphas=['asymmetric', 0.01, 0.1, 0.3, 0.5, 0.7], etas=['auto', 0.01, 0.1, 0.3])
