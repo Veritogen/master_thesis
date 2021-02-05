d662ec522e150d5c2a3a27f1963376054ef06fe2
Hannes Engelhardt <hannes.engelhardt@rwth-aachen.de>
2021-02-05 12:44:24 +0000

    modified min size to be a int

--- run_pol_lda_mallet.py
+++ run_pol_lda_mallet.py
@@ -33,5 +33,5 @@ filter_array = np.logical_and(stat_df.language == 'en',
 
 print(f"{len(filter_array)} is limiting to {sum(filter_array)}")
 nlp.preprocess(load_existing=True, filter_loaded=filter_array)
-nlp.create_bag_of_words(filter_extremes=True, min_df=(0.001*sum(filter_array)), max_df=0.3)
+nlp.create_bag_of_words(filter_extremes=True, min_df=(int(0.001*sum(filter_array))), max_df=0.3)
 nlp.search_best_model_mallet(topic_list=list(range(5, 100, 5)))
